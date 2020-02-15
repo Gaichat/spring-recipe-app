ba4f2f7b57ca49656a65ce18afbe3b68b1645433
maeliss vns <maeliss.avenoso@gmail.com>
2020-02-15 20:52:35 +0100

    create a new recipe

--- src/main/java/main/peppa/springframework/recipe/controllers/RecipeController.java
+++ src/main/java/main/peppa/springframework/recipe/controllers/RecipeController.java
@@ -1,9 +1,12 @@
 package main.peppa.springframework.recipe.controllers;
 
+import main.peppa.springframework.recipe.commands.RecipeCommand;
 import main.peppa.springframework.recipe.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,6 +22,18 @@ public class RecipeController {
     public String showRecipe(@PathVariable String id, Model model){
         model.addAttribute("recipe",recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("recipe/")
+    public String createRecipe(@ModelAttribute RecipeCommand recipeCommand){
+        RecipeCommand created = recipeService.saveRecipeCommand(recipeCommand);
+        return "redirect:show/"+created.getId();
     }
 
 
--- src/test/java/main/peppa/springframework/recipe/controllers/RecipeControllerTest.java
+++ src/test/java/main/peppa/springframework/recipe/controllers/RecipeControllerTest.java
@@ -1,5 +1,6 @@
 package main.peppa.springframework.recipe.controllers;
 
+import main.peppa.springframework.recipe.commands.RecipeCommand;
 import main.peppa.springframework.recipe.model.Recipe;
 import main.peppa.springframework.recipe.services.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,13 +9,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.mockito.Mockito.when;
 
@@ -29,6 +33,8 @@ class RecipeControllerTest {
     RecipeService recipeService;
 
     Recipe recipe;
+
+    RecipeCommand recipeCommand;
 
     MockMvc mockMvc;
 
@@ -51,5 +57,27 @@ class RecipeControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("recipe/show"))
                 .andExpect(model().attributeExists("recipe"));
+    }
+
+    void newRecipe() throws Exception {
+        mockMvc.perform(get("recipe/new"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/recipeform"))
+                .andExpect(model().attributeExists("recipe"));
+    }
+
+    void createRecipe() throws Exception {
+        RecipeCommand command = new RecipeCommand();
+        command.setId(2L);
+
+        when(recipeService.saveRecipeCommand(any())).thenReturn(command);
+
+        mockMvc.perform(post("/recipe")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("id", "")
+                .param("description", "some string")
+        )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:show/2"));
     }
 }
